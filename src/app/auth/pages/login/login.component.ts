4b72a2e5bc1caf0a95ff512a59d3e5dae14487a1
Ruben <rgarcia@odec.es>
2023-01-04 11:44:59 +0100

    App acabada

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
+import Swal from "sweetalert2";
+
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,15 +15,25 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 export class LoginComponent {
 
   miFormulario: FormGroup = this.fb.group({
-    email: ['test@test.com', [ Validators.required, Validators.email ]],
-    password: ['123123', [ Validators.required, Validators.minLength(6) ]]
+    email: ['', [ Validators.required, Validators.email ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]]
   });
 
-  constructor( private fb: FormBuilder ) { }
+  constructor( private fb: FormBuilder,
+               private router: Router,
+               private authService: AuthService ) { }
 
   login() {
-    console.log(this.miFormulario.value);
-    console.log(this.miFormulario.valid);
+    const { email, password } = this.miFormulario.value;
+
+    this.authService.login( email, password )
+      .subscribe( ok => {
+        if ( ok === true ) {
+          this.router.navigateByUrl('/dashboard');
+        } else {
+          Swal.fire('Error', ok, 'error' );
+        }
+      })
   }
 
 }
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
+
+import Swal from 'sweetalert2';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -10,16 +15,28 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 export class RegisterComponent {
 
   miFormulario: FormGroup = this.fb.group({
-    name: ['Ruben', [ Validators.required, Validators.minLength(2) ]],
-    email: ['test@test.com', [ Validators.required, Validators.email ]],
-    password: ['123123', [ Validators.required, Validators.minLength(6) ]]
+    name: ['', [ Validators.required, Validators.minLength(2) ]],
+    email: ['', [ Validators.required, Validators.email ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]]
   })
 
-  constructor( private fb: FormBuilder ) { }
+  constructor( private fb: FormBuilder,
+               private router: Router,
+               private authService: AuthService ) { }
 
   register() {
-    console.log(this.miFormulario.value);
-    console.log(this.miFormulario.valid);
+    
+    const { name, email, password } = this.miFormulario.value;
+
+    this.authService.register( name, email, password )
+      .subscribe( ok => {
+        if ( ok === true ) {
+          this.router.navigateByUrl('/dashboard');
+        } else {
+          Swal.fire('Error', ok, 'error');
+        }
+      })
+
   }
 
 }
